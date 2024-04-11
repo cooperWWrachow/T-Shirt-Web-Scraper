0f135b6ab354c3333ad491c024d3494a1a3b540d
Cooper Rachow <cooper.rachow@icloud.com>
2024-04-11 15:04:05 -0500

    Updated
    Has bug that does allow webscraping when 500+ objects available

--- boohoo/web_scraping.py
+++ boohoo/web_scraping.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,17 +14,30 @@ per_page = 80
 items_found = {}
 
 for start in range(0, item_count, per_page):
-    url = f"https://www.boohooman.com/us/search?q=oversized%20tshirt&prefn1=classification&prefv1=boohooMAN%20Tall%7CMain%20Collection&prefn2=sizeRefinement&prefv2=XL&start={start}&sz={per_page}"
+    url = f"https://www.boohooman.com/us/search?q=oversized%20tshirt&prefn1=classification&prefv1=boohooMAN%20Tall%7CMain%20Collection&prefn2=sizeRefinement&prefv2={size}&start={start}&sz={per_page}"
     page = requests.get(url).text
     doc = BeautifulSoup(page, "html.parser")
 
-    div = doc.find_all(class_="product-tile-name")
+    div = doc.find(class_="search-result-content js-search-result-content")
+
+    # all instances where there is a string within a tag that states "oversized"
+    items = div.find_all(string = re.compile("Oversized"))
+    
+    for item in items:
+        # title of product is WITHIN an <a> tag so that is the parent
+        parents = item.parent.parent.parent
+        link_class = parents.find(class_="product-image js-product-image load-bg").a
+        link = "https://www.boohooman.com" + link_class['data-href']
+        
+        next_parent = item.find_parent(class_="grid-tile")
+
+        try:
+            sale_price = next_parent.find(class_="product-sales-price product-sales-price-percent")
+            price = int(sale_price.contents[0].strip().replace("$", "")[:-3])
+            items_found[item] = {"price": price, "link": link}
+        except:
+            pass
+        
+print(items_found)
 
     
-
-url = f"https://www.boohooman.com/us/search?q=oversized%20tshirt&prefn1=classification&prefv1=boohooMAN%20Tall%7CMain%20Collection&prefn2=sizeRefinement&prefv2=XL&start=0&sz=80"
-page = requests.get(url).text
-doc = BeautifulSoup(page, "html.parser")
-
-div = doc.find_all(class_="product-tile-name")
-print(div)
