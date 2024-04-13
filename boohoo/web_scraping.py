0f76497c137d689f6a0ae70cdab516841c452ff4
Cooper Rachow <cooper.rachow@icloud.com>
2024-04-12 21:52:25 -0500

    Excel implemented

--- boohoo/web_scraping.py
+++ boohoo/web_scraping.py
@@ -1,9 +1,12 @@
 import re
 from bs4 import BeautifulSoup
 import requests
+import openpyxl
+from openpyxl.styles import Alignment, Font
+import os
 
 
-user_input  = input("input size, max price, and a color with a space in between each: ")
+user_input  = input("input size, max price, and a color (optional) with a space in between each: ")
 filters = user_input.split()
 
 if len(filters) == 3:
@@ -27,7 +30,6 @@ total_pages = int(doc.find(class_="pagination-info js-pagination-info hidden-on-
 per_page = 80
 
 items_found = {}
-count = 0
 
 # iterate through each page based on total total items per page and page count
 for page in range(total_pages):
@@ -59,16 +61,59 @@ for page in range(total_pages):
             price = int(sale_price.contents[0].strip().replace("$", "")[:-3])
             if price <= max_price:
                 items_found[item.replace("\n", "")] = {"price": price, "link": link}
-                count += 1
             else:
                 continue
         except:
             pass
-        
-print(items_found)
-print(f"There are {count} items on sale!")
+
 
 # place into excel
 
+workbook = openpyxl.load_workbook('boohoo.xlsx')
 
-    
+std = workbook['Sheet1']
+
+# clears entire spreadsheet
+for i in std.iter_rows():
+    for cell in i:
+        cell.value = None
+
+
+# populate headers
+col_titles = ['Name', 'Price', 'URL']
+for column_index, title in enumerate(col_titles, start=1):
+    header = std.cell(row=1, column=column_index, value=title)
+    header.alignment = Alignment(horizontal='center', vertical='center')
+
+
+count = 0
+# populate with data 
+for row_index, (product, details) in enumerate(items_found.items(), start=3):
+    name_cell = std.cell(row=row_index, column=1, value=product) 
+    price_cell = std.cell(row=row_index, column=2, value=details['price'])  
+    url_cell = std.cell(row=row_index, column=3, value=details['link'])  
+
+    # adjust width of columns A and C for name and link
+    product_width = len(product) + 2  
+    std.column_dimensions['A'].width = max(std.column_dimensions['A'].width, product_width)
+
+    price_cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    link_width = len(details['link']) + 2 
+    std.column_dimensions['C'].width = max(std.column_dimensions['C'].width, link_width)
+    url_cell.hyperlink = details['link']
+    url_cell.font = Font(color='0000ff', underline='single')
+    count += 1
+
+# set the count variable then save 
+count_cell = std['D1']
+count_cell.alignment = Alignment(horizontal='center', vertical='center')
+message = f"Count={count}"
+cell_width = len(message) + 2
+count_cell.value = message
+std.column_dimensions['D'].width = cell_width
+
+workbook.save('boohoo.xlsx')
+
+os.startfile('C:/Users/coope/Desktop/GitHub/T-Shirt-Web-Scraper/boohoo/boohoo.xlsx')
+
